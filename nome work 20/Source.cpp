12fb59a53f7d99ba750a3a21ee3fd5705e095b7b
vadshos <va.vy.shostak@gmail.com>
2020-07-03 13:28:13 +0300

    first commit

---
+++ nome work 20/Source.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+using namespace std;
+
+	
+
+float plug(float a, float b) {
+   float result = a + b;
+    
+    return result;
+}
+float minu(float a, float b) {
+   float result = a - b;
+    
+    return result;
+}
+float plural(float a, float b) {
+    float result = a * b;
+    
+    return result;
+}
+float dil(float a, float b) {
+     float result = a / b;
+    
+    return result;
+}
+
+int main() {
+    float a1;
+    float b1;
+    char action;
+    cout <<" enter an example in itself number action number end press enter"<<endl;
+    cin >> a1 >>action>>b1;
+    
+    
+    
+    float suma;
+    switch (action)
+    {
+    case '+':
+       suma =  plug(a1,b1);
+       cout << suma;
+        break;
+
+    case '-':
+        suma = minu(a1,b1);
+        cout << suma;
+        break;
+
+    case '*':
+        suma = plural(a1,b1);
+        cout << suma;
+        break;
+
+    case '/':
+         suma = dil(a1,b1);
+         cout << suma;
+        break;
+
+    }
+}
+
